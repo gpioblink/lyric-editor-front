3a3c2693e9a6d1ed74a7202f2b8172227d4748ee
Kai Yamada <gpioblink@aol.com>
2020-03-12 10:52:47 +0900

    add action

---
+++ src/Redux/Actions.ts
@@ -0,0 +1,45 @@
+import {PlayStatus, Point} from "./Store";
+
+export type Action =
+    | {
+        type: 'IMPORT_FME';
+        fmeDataBase64: string;
+      }
+    | {
+        type: 'NEXT_PAGE';
+      }
+    | {
+        type: 'PREVIOUS_PAGE';
+      }
+    | {
+        type: 'PLAY_STATUS';
+        playState: PlayStatus;
+      }
+    | {
+        type: 'CHANGE_LINE_POINT';
+        index: number;
+        point: Point;
+      }
+    | {
+        type: 'CHANGE_LINE_CHAR';
+        index: number;
+        string: string;
+      }
+    | {
+        type: 'CHANGE_CHAR_TIME';
+        lineIndex: number;
+        charIndex: number;
+        time: number;
+      }
+    | {
+        type: 'CHANGE_RUBY_POINT';
+        lineIndex: number;
+        rubyIndex: number;
+        fedx: number;
+      }
+    | {
+        type: 'CHANGE_RUBY';
+        lineIndex: number;
+        rubyIndex: number;
+        string: string;
+      };
